439bacfb15a753118b18a34323dfebeb243fb5e1
Brian <brianhilsden@gmail.com>
2024-11-12 23:53:15 +0300

    updated database to supabase

--- lms/wsgi.py
+++ lms/wsgi.py
@@ -14,4 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lms.settings')
 
 # Change to app for vercel deployment, and application to work locally
-app = get_wsgi_application()
+application = get_wsgi_application()
